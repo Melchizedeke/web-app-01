b71fd91afbca7300252bba5dddc31d62ba29a319
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2023-09-07 10:32:33 +0100

    Code changes

--- learninglog_env/learning_logs/models.py
+++ learninglog_env/learning_logs/models.py
@@ -11,14 +11,14 @@ class Topic(models.Model):
         return self.text
     
 class Entry(models.Model):
-    """Something Specific Learned About the Topic"""
+    """Something specific learned about a topic"""
     topic = models.ForeignKey(Topic)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name_plural = "entries"
+        verbose_name_plural = 'entries'
 
     def __str__(self):
-        """Returns a string representation of the model"""
+        """Return a string representation of the model."""
         return self.text[:50] + "..."
