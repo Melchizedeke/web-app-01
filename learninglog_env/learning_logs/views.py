41b2fbc2b6c918c132985680f381c86d8229308b
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2023-09-08 16:48:58 +0100

    code changes

--- learninglog_env/learning_logs/views.py
+++ learninglog_env/learning_logs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# def homePageView(request):
+#     return HttpResponse("My New App!")
+
 def homePageView(request):
-    return HttpResponse("My New App!")
+    """The home page for Learning Log"""
+    return render(request, 'learning_logs/index.html')
