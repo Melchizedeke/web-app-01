30403bcdd8e055f38434e4f679ece4b44f064099
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2023-09-13 15:14:06 +0100

    Updates
    Updated the urls.py file for the learning_logs app

--- learninglog_env/learning_logs/urls.py
+++ learninglog_env/learning_logs/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    # Home Page
     path("", views.learning_logs_home, name="learning_logs_home"),
+    
+    # Show all Topics
+    path("topics/", views.topics, name='topics'),
 ]
