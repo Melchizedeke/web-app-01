5ca26c9e2855bf9ae84b526698f85f8e97715544
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2023-09-13 15:20:46 +0100

    New code
    New lines of code for some parts of the app

--- learninglog_env/learning_logs/views.py
+++ learninglog_env/learning_logs/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render,HttpResponse
+from .models import Topic
 
 def learning_logs_home(request):
     """The home page for Learning Log"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Show all topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics':topics}
+    return render(request=request, 'learning_logs.html', context=context)
